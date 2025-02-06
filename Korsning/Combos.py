584c68bc3b789f359523052c1d2043441a1b9561
David <github@dbystrom.com>
2025-02-06 15:27:32 +0100

    Klar för idag

--- Korsning/Combos.py
+++ Korsning/Combos.py
@@ -1,7 +1,7 @@
 import itertools
 
 # Define the characters to use for combinations
-letters = ['G', 'r', 'y']
+letters = ['G', 'r']
 
 # Generate all combinations of length 6
 combinations = [''.join(comb) for comb in itertools.product(letters, repeat=6)]
